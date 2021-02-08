8d2e4140300a464df1253c090051137822782428
Ronald Lima <ronaldazevedolima@gmail.com>
2021-02-08 18:03:44 -0300

    layout inicial pag login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from '../components/Header';
 
 const Login = () => (
-    <div>
-      <Header />
-      <h1>login</h1>
-    </div>
-  );
+  <div>
+    <Header />
+    <h1>login</h1>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Endereço de email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Seu email"
+        />
+        <small id="emailHelp" class="form-text text-muted">
+          Nunca vamos compartilhar seu email, com ninguém.
+        </small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Senha</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Senha"
+        />
+      </div>
+      <Link to='/home'>
+        <button type="submit" class="btn btn-primary">
+          Enviar
+        </button>
+      </Link>
+    </form>
+  </div>
+);
 
-  export default Login;
-  
+export default Login;
