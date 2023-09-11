047e177d02bebcae2b2457169fcbf0ab41aaa559
ronaldazevedolima <ronaldazevedolima@gmail.com>
2023-09-11 20:28:59 -0300

    atualizando react para next

--- front-end/src/pages/Index.js
+++
@@ -1,5 +0,0 @@
-export { default as Login } from './Login';
-export { default as Books } from './Books';
-export { default as Collections } from './Collections';
-export { default as About } from './About';
-export { default as Authors } from './Authors';
