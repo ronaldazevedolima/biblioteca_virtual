a7012a221b08c3af905dc078ccf2c5c2c15706c4
Ronald Lima <ronaldlima@MacBook-Pro-de-Ronald.local>
2025-02-13 17:50:17 -0300

    Atualização/retirada de lib obsoleta

--- back-end/src/config/config.js
+++ back-end/src/config/config.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
-
 const config = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  host: process.env.HOSTNAME || 'localhost',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: 3306,
   dialect: 'mysql',
 };
 
