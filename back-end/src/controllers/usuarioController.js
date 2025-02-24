d6facb6b50281210ab3625b6b44559f7ad6a472a
Ronald Lima <ronaldlima@MacBook-Pro-de-Ronald.local>
2025-02-24 15:57:48 -0300

    Refactor/aplicando feedbacks recebidos

--- back-end/src/controllers/usuarioController.js
+++ back-end/src/controllers/usuarioController.js
@@ -1,45 +1,45 @@
-const { usurioService } = require('../service');
+const { usuarioService } = require('../service');
 
 const tdsUsrs = async (req, res) => {
-  const { status, resposta } = await usurioService.tdsUsrs();
+  const { status, resposta } = await usuarioService.tdsUsrs();
   return res.status(status).json(resposta);
 };
 
 const usrId = async (req, res) => {
   const { id } = req.params;
-  const { status, resposta } = await usurioService.usrId(id);
+  const { status, resposta } = await usuarioService.usrId(id);
   return res.status(status).json(resposta);
 };
 
 const criaUsr = async (req, res) => {
   const usrInfo = req.body;
-  const { status, resposta } = await usurioService.criaUsr(usrInfo);
+  const { status, resposta } = await usuarioService.criaUsr(usrInfo);
   return res.status(status).json(resposta);
 };
 
 const atlizUsuario = async (req, res) => {
   const userInfo = req.body;
   const { id } = req.params;
-  const { status, resposta } = await usurioService.atlizUsuario(id, userInfo);
+  const { status, resposta } = await usuarioService.atlizUsuario(id, userInfo);
   return res.status(status).json(resposta);
 };
 
 const atlzClassficacao = async (req, res) => {
   const { id } = req.params;
   const { classificacao } = req.body;
-  const { status, resposta } = await usurioService.atlzClassficacao(id, classificacao);
+  const { status, resposta } = await usuarioService.atlzClassficacao(id, classificacao);
   return res.status(status).json(resposta);
 };
 
 const delUsr = async (req, res) => {
   const { id } = req.params;
-  const { status, resposta } = await usurioService.delUsr(id);
+  const { status, resposta } = await usuarioService.delUsr(id);
   return res.status(status).json(resposta);
 };
 
 const efetuarLogin = async (req, res) => {
   const { email, senha } = req.body;
-  const { status, resposta } = await usurioService.efetuarLogin(email, senha);
+  const { status, resposta } = await usuarioService.efetuarLogin(email, senha);
   return res.status(status).json(resposta);
 };
 
